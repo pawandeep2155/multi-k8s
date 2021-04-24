2c8f8affc6cf8686758e3af2dc63f924fd53d8bd
Pawandeep Singh <pawandeep@madstreetden.com>
2021-04-24 09:51:59 +0530

    kubernetes config files added

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React in Kubernetes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
